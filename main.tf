ace73a1716a3109bbfb9566cc7f5fabf78a813c2
osanni <42106750+Kunlesanni@users.noreply.github.com>
2022-06-26 15:04:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,33 +16,9 @@ resource "azurerm_firewall_policy_rule_collection_group" "rcgname" {
     name     = "azure_dcs"
     priority = 400
     action   = "Deny"
-    rule {
-      name                  = "in_T_I_azdc_addev"
-      protocols             = ["TCP", "UDP"]
-      source_addresses      = ["10.0.0.1"]
-      destination_addresses = ["192.168.1.1", "192.168.1.2"]
-      destination_ports     = ["80", "1000-2000"]
-    }
-    rule {
-      name                  = "in_U_I_azdc_addev"
-      protocols             = ["TCP", "UDP"]
-      source_addresses      = ["10.0.0.1"]
-      destination_addresses = ["192.168.1.1", "192.168.1.2"]
-      destination_ports     = ["80", "1000-2000"]
-    }
-    rule {
-      name                  = "in_T_I_5722_azdc_addev"
-      protocols             = ["TCP", "UDP"]
-      source_addresses      = ["10.0.0.1"]
-      destination_addresses = ["192.168.1.1", "192.168.1.2"]
-      destination_ports     = ["80", "1000-2000"]
-    }
-    rule {
-      name                  = "out_T_I_5722_azdc_addev"
-      protocols             = ["TCP", "UDP"]
-      source_addresses      = ["10.0.0.1"]
-      destination_addresses = ["192.168.1.1", "192.168.1.2"]
-      destination_ports     = ["80", "1000-2000"]
-    }
-    
+    rule = var.rule1
+    rule = var.rule2
+    rule = var.rule3
+    rule = var.rule4   
   }
+}
