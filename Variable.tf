2023df6d07b136f846c255d22ce3483cf1a1b02b
osanni <42106750+Kunlesanni@users.noreply.github.com>
2022-06-26 14:27:25 +0100

    Create Variable.tf

---
+++ Variable.tf
@@ -0,0 +1,28 @@
+rule {
+      name                  = "in_T_I_azdc_addev"
+      protocols             = ["TCP", "UDP"]
+      source_addresses      = ["10.0.0.1"]
+      destination_addresses = ["192.168.1.1", "192.168.1.2"]
+      destination_ports     = ["80", "1000-2000"]
+    }
+    rule {
+      name                  = "in_U_I_azdc_addev"
+      protocols             = ["TCP", "UDP"]
+      source_addresses      = ["10.0.0.1"]
+      destination_addresses = ["192.168.1.1", "192.168.1.2"]
+      destination_ports     = ["80", "1000-2000"]
+    }
+    rule {
+      name                  = "in_T_I_5722_azdc_addev"
+      protocols             = ["TCP", "UDP"]
+      source_addresses      = ["10.0.0.1"]
+      destination_addresses = ["192.168.1.1", "192.168.1.2"]
+      destination_ports     = ["80", "1000-2000"]
+    }
+    rule {
+      name                  = "out_T_I_5722_azdc_addev"
+      protocols             = ["TCP", "UDP"]
+      source_addresses      = ["10.0.0.1"]
+      destination_addresses = ["192.168.1.1", "192.168.1.2"]
+      destination_ports     = ["80", "1000-2000"]
+    }
