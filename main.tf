1bb6e65d74492877636b269cf5ad875c1cf045f6
osanni <42106750+Kunlesanni@users.noreply.github.com>
2022-06-26 15:17:32 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  alias           = "hubc"
+  subscription_id = "effec6ac-0c2f-40d8-ac3e-7ea8f2116d46"
+  tenant_id       = "1faf88fe-a998-4c5b-93c9-210a11d9a5c2"
+  client_id       = "8a0a2e91-fbc1-4606-bc1f-d43c84fd2aa8"
+  client_secret   = "Cvc8Q~TFVrkTkElEoG58NsiOwHn5EjJxC6YqLbgY"
+  features {}
+}
+
 data "azurerm_resource_group" "rgdata" {
   name = "rg"
 }
